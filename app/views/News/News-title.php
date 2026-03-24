74c448fde09823aadedd576a7e3fbe0772998b8b
PhucMinh0303 <phucminh0303@gmail.com>
2026-03-24 15:49:05 +0700

    update views (News)

--- app/views/News/News-title.php
+++ app/views/News/News-title.php
@@ -3,6 +3,7 @@ $page_title = "Chi tiết tin tức";
 ?>
 <?php include VIEWS_PATH . 'pages/include/head-root.php'; ?>
 <?php include VIEWS_PATH . 'pages/include/header.php'; ?>
+<?php include VIEWS_PATH . 'pages/News/news-section.php'; ?>
 <?php include VIEWS_PATH . 'pages/News/news-title.php'; ?>
 <?php include VIEWS_PATH . 'pages/include/footer.php'; ?>
 <?php include VIEWS_PATH . 'pages/include/scripts-root.php'; ?>
