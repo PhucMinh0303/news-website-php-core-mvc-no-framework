2f7e6c9b6f9a116c0c317958fc9170d6e381317d
PhucMinh0303 <phucminh0303@gmail.com>
2026-03-24 15:49:53 +0700

    update views (News)

--- app/views/pages/News/news-title.php
+++ app/views/pages/News/news-title.php
@@ -2,111 +2,115 @@
 /**
  * News Title Page - Article Detail View
  */
+// views/news/detail.php
+
 ?>
 <main class="section10">
-  <section class="f_page r_p6">
-    <div class="min_wrap-news">
-      <article class="ct_page">
-        <h1 class="til_news_D">
-          Đầu tư tài chính với số vốn nhỏ - Nên hay không?
-        </h1>
-        <div class="bot_til_td_D">
-          <p class="dc_td_D">
+    <section class="f_page r_p6">
+        <div class="min_wrap-news">
+            <article class="ct_page">
+                <h1 class="til_news_D">
+                    <?php echo htmlspecialchars($news['title']); ?>
+                </h1>
+                <div class="bot_til_td_D">
+                    <p class="dc_td_D">
             <span>
               <i class="fa-solid fa-calendar-week"></i>
-              17/11/2025 10:30
+              <?php echo date('d/m/Y H:i', strtotime($news['published_at'] ?? $news['publish_date'])); ?>
             </span>
-            <span>
+                        <span>
               <i class="fa-regular fa-eye"></i>
-              8
+              <?php echo number_format($news['views']); ?>
             </span>
-          </p>
-          <div class="share_D">
-            <span>Share</span>
-            <ul class="list_share_D">
-              <li>
-                <a
-                  class="copy_links"
-                  href="javascript:void(0)"
-                  title="Copy link"
-                  val="<?php echo View::url('news/dau-tu-tai-chinh-voi-so-von-nho-nen-hay-khong'); ?>"
-                >
-                  <i class="fa-solid fa-link"></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(BASE_URL . 'news/dau-tu-tai-chinh-voi-so-von-nho-nen-hay-khong'); ?>"
-                  target="_blank"
-                  title="Chia sẻ bài viết lên Facebook"
-                >
-                  <i class="fa-brands fa-facebook-f"></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href="http://www.twitter.com/share?url=<?php echo urlencode(BASE_URL . 'news/dau-tu-tai-chinh-voi-so-von-nho-nen-hay-khong'); ?>"
-                  target="_blank"
-                  title="Chia sẻ bài viết lên Twitter"
-                >
-                  <i class="fa-brands fa-twitter"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
+                    </p>
+                    <div class="share_D">
+                        <span>Share</span>
+                        <ul class="list_share_D">
+                            <li>
+                                <a
+                                        class="copy_links"
+                                        href="javascript:void(0)"
+                                        title="Copy link"
+                                        val="<?php echo $currentUrl; ?>"
+                                        onclick="copyToClipboard(this.getAttribute('val'))">
+                                    >
+                                    <i class="fa-solid fa-link"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                        href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($currentUrl); ?>&t=<?php echo urlencode($news['title']); ?>"
+                                        target="_blank"
+                                        title="Chia sẻ bài viết lên Facebook"
+                                        onclick="trackShare('facebook', <?php echo $news['news_id']; ?>)">
+                                    >
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                        href="http://www.twitter.com/share?url=<?php echo urlencode($currentUrl); ?>&text=<?php echo urlencode($news['title']); ?>"
+                                        target="_blank"
+                                        title="Chia sẻ bài viết lên Twitter"
+                                        onclick="trackShare('twitter', <?php echo $news['news_id']; ?>)">
+                                    >
+                                    <i class="fa-brands fa-twitter"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Author Info -->
+                <?php if (!empty($news['author_name'])): ?>
+                    <div class="author-info">
+                        <div class="author-avatar">
+                            <?php if ($news['author_avatar']): ?>
+                                <img src="<?php echo htmlspecialchars($news['author_avatar']); ?>"
+                                     alt="<?php echo htmlspecialchars($news['author_name']); ?>">
+                            <?php else: ?>
+                                <i class="fa-solid fa-user-circle"></i>
+                            <?php endif; ?>
+                        </div>
+                        <div class="author-details">
+                            <strong><?php echo htmlspecialchars($news['author_name']); ?></strong>
+                            <?php if ($news['author_bio']): ?>
+                                <p><?php echo htmlspecialchars($news['author_bio']); ?></p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+                <h2 class="des_news_D">
+                    <?php echo htmlspecialchars($news['description']); ?>
+                </h2>
+                <!-- Article Content -->
+                <div class="f-detail clearfix">
+                    <?php if ($news['featured_image']): ?>
+                        <div class="featured-image">
+                            <img alt="<?php echo htmlspecialchars($news['title']); ?>"
+                                 src="<?php echo htmlspecialchars($news['featured_image']); ?>"
+                                 style="width: 100%; max-width: 800px; height: auto;"/>
+                            <?php if ($news['featured_image_caption']): ?>
+                                <p class="image-caption"><?php echo htmlspecialchars($news['featured_image_caption']); ?></p>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo $news['content']; ?>
+                </div>
+                <!-- Tags -->
+                <?php if (!empty($tags)): ?>
+                    <div class="news-tags">
+                        <strong><i class="fa-solid fa-tags"></i> Tags:</strong>
+                        <?php foreach ($tags as $tag): ?>
+                            <a href="/news/tag/<?php echo htmlspecialchars($tag['slug']); ?>" class="tag-link">
+                                #<?php echo htmlspecialchars($tag['name']); ?>
+                            </a>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </article>
         </div>
-        <h2 class="des_news_D">
-          Trên thị trường hiện có nhiều hình thức đầu tư giúp gia tăng số tiền
-          nhanh chóng. Tuy nhiên, để có được lợi nhuận cao thường đòi hỏi việc
-          đầu tư, kinh doanh số vốn khá lớn. Do đó, rất nhiều người đặt ra câu
-          hỏi "Có thể đầu tư tài chính với số vốn nhỏ không?"
-        </h2>
-        <div class="f-detail clearfix">
-          <img
-            alt=""
-            src="https://demo29.escovietnam.vn/capitalAM/uploads/noidung/images/baiviet/unnamed-18.png"
-            style="width: 100%"
-          />
-          <p>
-            Trên thị trường hiện có nhiều hình thức đầu tư giúp gia tăng số tiền
-            nhanh chóng. Tuy nhiên, để có được lợi nhuận cao thường đòi hỏi việc
-            đầu tư, kinh doanh số vốn khá lớn. Do đó, rất nhiều người đặt ra câu
-            hỏi "Có thể đầu tư tài chính với số vốn nhỏ không?"
-          </p>
-
-          <ol>
-            <li><b>Có nên đầu tư tài chính với số vốn nhỏ không?</b></li>
-          </ol>
-
-          <p>
-            Bất kỳ nhà đầu tư tên tuổi nào đều khởi đầu bằng việc đầu tư với số
-            vốn khiêm tốn. Đây là những bước đầu tiên để họ học hỏi kinh nghiệm,
-            tiếp thu kiến thức đầu tư trước khi tiến xa hơn trên con đường đầu
-            tư tài chính với những khoản đầu tư lớn hơn.
-          </p>
-          <br />
-
-          <p>
-            Hãy ghi nhớ, hiệu quả của đầu tư sẽ không bị ảnh hưởng quá nhiều bởi
-            khoản vốn bạn bỏ ra, nó sẽ phụ thuộc vào thời gian bạn dành cho việc
-            đầu tư, lĩnh vực bạn quan tâm và tỷ lệ lợi nhuận bạn có thể đạt
-            được…
-          </p>
-          <br />
-
-          <p>
-            Việc đầu tư tài chính với số vốn nhỏ mang đến nhiều lợi ích cho nhà
-            đầu tư:
-          </p>
-
-          <ul>
-            <li aria-level="1">
-              Có nhiều ngành nghề, lĩnh vực để nhà đầu tư vốn nhỏ có thể cân
-              nhắc: gửi tiết kiệm, bảo hiểm đầu tư, cổ phiếu, trái phiếu hay
-            </li>
-          </ul>
-        </div>
-      </article>
-    </div>
-  </section>
+    </section>
 </main>
