52c2a26e9db601047ab96ebe7bf5c3d1835d6dd5
PhucMinh0303 <phucminh0303@gmail.com>
2026-03-25 15:54:45 +0700

    update views

--- app/views/pages/Recruitment/recruitment.php
+++ app/views/pages/Recruitment/recruitment.php
@@ -1,246 +1,98 @@
 <?php
-/**
- * Recruitment Listing Page - All Job Positions
- */
+// app/views/recruitment/index.php
 ?>
-<main class="section10">
-  <section class="f_td r_p36">
-    <div class="min_wrap_recruitment">
-      <ul class="list_td">
-        <li>
-          <div class="c1_list_td">
-            <a href="<?php echo View::url('recruitment/truong-phong-nguon-von'); ?>" title="Trưởng phòng nguồn vốn">
-              <figure class="img_list_td">
-                <img
-                  src="<?php echo View::asset('img/recruitment/truong-phong-nguon-von-1763953822-egprx.webp'); ?>"
-                  alt="Trưởng phòng nguồn vốn"
-                />
-              </figure>
-            </a>
+    <main class="section10">
+        <section class="f_td r_p36">
+            <div class="min_wrap_recruitment">
 
-            <div class="if_list_td">
-              <h3 class="na_list_td link_hv">
-                <a
-                  href="<?php echo View::url('recruitment/truong-phong-nguon-von'); ?>"
-                  class="link_hv"
-                  title="Trưởng phòng nguồn vốn"
-                  >Trưởng phòng nguồn vốn</a
-                >
-              </h3>
+                <?php if (isset($recruitments) && count($recruitments) > 0): ?>
+                    <ul class="list_td">
+                        <?php foreach ($recruitments as $job): ?>
+                            <li>
+                                <div class="c1_list_td">
+                                    <a href="<?php echo $this->url('recruitment/' . $job['slug']); ?>"
+                                       title="<?php echo htmlspecialchars($job['recruitment_title']); ?>">
+                                        <figure class="img_list_td">
+                                            <img
+                                                    src="<?php echo $this->asset('img/recruitment/' . ($job['image'] ?? 'default-job.webp')); ?>"
+                                                    alt="<?php echo htmlspecialchars($job['recruitment_title']); ?>"
+                                            />
+                                        </figure>
+                                    </a>
 
-              <p>
-                Nơi làm việc: Hội sở: Hasco Building, 98 Xuân Thủy, Phường An
-                Khánh, Tp. Hồ Chí Minh
-              </p>
+                                    <div class="if_list_td">
+                                        <h3 class="na_list_td link_hv">
+                                            <a href="<?php echo $this->url('recruitment/' . $job['slug']); ?>"
+                                               class="link_hv"
+                                               title="<?php echo htmlspecialchars($job['recruitment_title']); ?>">
+                                                <?php echo htmlspecialchars($job['recruitment_title']); ?>
+                                            </a>
+                                        </h3>
 
-              <ol>
-                <li>
-                  Bằng cấp:
+                                        <p>Nơi làm việc: <?php echo htmlspecialchars($job['location']); ?></p>
 
-                  <strong>Cao Đẳng - Đại Học</strong>
-                </li>
+                                        <ol>
+                                            <li>
+                                                Bằng cấp:
+                                                <strong><?php echo htmlspecialchars($job['education'] ?? 'Cao Đẳng - Đại Học'); ?></strong>
+                                            </li>
+                                            <li>
+                                                Số lượng tuyển:
+                                                <strong><?php echo $job['quantity']; ?></strong>
+                                            </li>
+                                            <?php if (!empty($job['salary_range'])): ?>
+                                                <li>
+                                                    Mức lương:
+                                                    <strong><?php echo htmlspecialchars($job['salary_range']); ?></strong>
+                                                </li>
+                                            <?php endif; ?>
+                                        </ol>
+                                    </div>
+                                </div>
 
-                <li>
-                  Số lượng tuyển:
+                                <div class="c2_list_td">
+                                    <div class="date_list_td">
+                                        <span>Hạn nộp hồ sơ</span>
+                                        <strong><?php echo date('d-m-Y', strtotime($job['deadline'])); ?></strong>
+                                    </div>
 
-                  <strong>01</strong>
-                </li>
-              </ol>
+                                    <div class="but_list_td">
+                                        <a href="<?php echo $this->url('recruitment/' . $job['slug']); ?>"
+                                           class="but_03"
+                                           title="<?php echo htmlspecialchars($job['recruitment_title']); ?>">
+                                            Xem chi tiết
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <!--end list_td-->
+                <?php else: ?>
+                    <div class="no-recruitment">
+                        <p>Hiện tại chưa có vị trí tuyển dụng nào. Vui lòng quay lại sau!</p>
+                    </div>
+                <?php endif; ?>
+
+                <div class="page">
+                    <div class="PageNum"></div>
+                    <div class="clear"></div>
+                </div>
             </div>
-          </div>
+            <!--end min_wrap-->
+        </section>
+    </main>
 
-          <div class="c2_list_td">
-            <div class="date_list_td">
-              <span>Hạn nộp hồ sơ</span>
+<?php
+// Helper methods for View
+function url($path)
+{
+    return '/' . ltrim($path, '/');
+}
 
-              <strong>22-11-2025</strong>
-            </div>
+function asset($path)
+{
+    return '/public/' . ltrim($path, '/');
+}
 
-            <div class="but_list_td">
-              <a
-                href="<?php echo View::url('recruitment/truong-phong-nguon-von'); ?>"
-                class="but_03"
-                title="Trưởng phòng nguồn vốn"
-              >
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
-        </li>
-
-        <li>
-          <div class="c1_list_td">
-            <a href="<?php echo View::url('recruitment/chuyen-vien-dau-tu'); ?>" title="Chuyên viên đầu tư">
-              <figure class="img_list_td">
-                <img
-                  src="<?php echo View::asset('img/recruitment/truong-phong-nguon-von-1763953822-egprx.webp'); ?>"
-                  alt="Chuyên viên đầu tư"
-                />
-              </figure>
-            </a>
-
-            <div class="if_list_td">
-              <h3 class="na_list_td link_hv">
-                <a href="<?php echo View::url('recruitment/chuyen-vien-dau-tu'); ?>" class="link_hv" title="Chuyên viên đầu tư"
-                  >Chuyên viên đầu tư</a
-                >
-              </h3>
-
-              <p>
-                Nơi làm việc: Hội sở: Hasco Building, 98 Xuân Thủy, Phường An
-                Khánh, Tp. Hồ Chí Minh
-              </p>
-
-              <ol>
-                <li>
-                  Bằng cấp:
-
-                  <strong>Cao Đẳng - Đại Học</strong>
-                </li>
-
-                <li>
-                  Số lượng tuyển:
-
-                  <strong>01</strong>
-                </li>
-              </ol>
-            </div>
-          </div>
-
-          <div class="c2_list_td">
-            <div class="date_list_td">
-              <span>Hạn nộp hồ sơ</span>
-
-              <strong>22-11-2025</strong>
-            </div>
-
-            <div class="but_list_td">
-              <a href="<?php echo View::url('recruitment/chuyen-vien-dau-tu'); ?>" class="but_03" title="Chuyên viên đầu tư">
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
-        </li>
-
-        <li>
-          <div class="c1_list_td">
-            <a
-              href="<?php echo View::url('recruitment/chuyen-vien-hanh-chinh'); ?>"
-              title="Chuyên viên hành chính"
-            >
-              <figure class="img_list_td">
-                <img
-                  src="<?php echo View::asset('img/recruitment/truong-phong-nguon-von-1763953822-egprx.webp'); ?>"
-                  alt="Chuyên viên hành chính"
-                />
-              </figure>
-            </a>
-
-            <div class="if_list_td">
-              <h3 class="na_list_td link_hv">
-                <a href="<?php echo View::url('recruitment/chuyen-vien-hanh-chinh'); ?>" class="link_hv" title="Chuyên viên hành chính"
-                  >Chuyên viên hành chính</a
-                >
-              </h3>
-
-              <p>
-                Nơi làm việc: Hội sở: Hasco Building, 98 Xuân Thủy, Phường An
-                Khánh, Tp. Hồ Chí Minh
-              </p>
-
-              <ol>
-                <li>
-                  Bằng cấp:
-
-                  <strong>Cao Đẳng - Đại Học</strong>
-                </li>
-
-                <li>
-                  Số lượng tuyển:
-
-                  <strong>01</strong>
-                </li>
-              </ol>
-            </div>
-          </div>
-
-          <div class="c2_list_td">
-            <div class="date_list_td">
-              <span>Hạn nộp hồ sơ</span>
-
-              <strong>22-11-2025</strong>
-            </div>
-
-            <div class="but_list_td">
-              <a href="<?php echo View::url('recruitment/chuyen-vien-hanh-chinh'); ?>" class="but_03" title="Chuyên viên hành chính">
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
-        </li>
-
-        <li>
-          <div class="c1_list_td">
-            <a href="<?php echo View::url('recruitment/chuyen-vien-nhan-su'); ?>" title="Chuyên viên nhân sự">
-              <figure class="img_list_td">
-                <img
-                  src="<?php echo View::asset('img/recruitment/truong-phong-nguon-von-1763953822-egprx.webp'); ?>"
-                  alt="Chuyên viên nhân sự"
-                />
-              </figure>
-            </a>
-
-            <div class="if_list_td">
-              <h3 class="na_list_td link_hv">
-                <a href="<?php echo View::url('recruitment/chuyen-vien-nhan-su'); ?>" class="link_hv" title="Chuyên viên nhân sự"
-                  >Chuyên viên nhân sự</a
-                >
-              </h3>
-
-              <p>
-                Nơi làm việc: Hội sở: Hasco Building, 98 Xuân Thủy, Phường An
-                Khánh, Tp. Hồ Chí Minh
-              </p>
-
-              <ol>
-                <li>
-                  Bằng cấp:
-
-                  <strong>Cao Đẳng - Đại Học</strong>
-                </li>
-
-                <li>
-                  Số lượng tuyển:
-
-                  <strong>01</strong>
-                </li>
-              </ol>
-            </div>
-          </div>
-
-          <div class="c2_list_td">
-            <div class="date_list_td">
-              <span>Hạn nộp hồ sơ</span>
-
-              <strong>21-11-2025</strong>
-            </div>
-
-            <div class="but_list_td">
-              <a href="<?php echo View::url('recruitment/chuyen-vien-nhan-su'); ?>" class="but_03" title="Chuyên viên nhân sự">
-                Xem chi tiết
-              </a>
-            </div>
-          </div>
-        </li>
-      </ul>
-      <!--end list_td-->
-
-      <div class="page">
-        <div class="PageNum"></div>
-
-        <div class="clear"></div>
-      </div>
-    </div>
-    <!--end min_wrap-->
-  </section>
-</main>
+?>
